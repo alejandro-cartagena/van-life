a76a272590600dfdc644168ea3ca7d1b32ef1629
Alejandro <alejandrocartagena68@gmail.com>
2024-04-07 17:51:11 -0400

    Compressed the bg img in the home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,7 +7,8 @@ export default function Home() {
     
         <div className="home-container">
             <div className="home-text-container">
-                <h1>You got the travel plans, we got the travel vans.</h1>
+                <h1>You got the travel plans</h1>
+                <h1>We got the travel vans</h1>
                 <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
                 <Link to="vans">Find your van</Link>
             </div>
